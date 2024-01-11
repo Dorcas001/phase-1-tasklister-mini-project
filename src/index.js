02f48625c561af5d17936a0c18a8c39be38d88a5
karimi <karimidorcas1@gmail.com>
2024-01-11 06:19:08 +0300

    partially complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.getElementById("create-task-form" );
+const todoList = document.querySelector('#tasks');
+const todoDiv = document.getElementById('list');
+const todoInput = document.getElementById('new-task-description');
+
+// add eventlistener
+form.addEventListener('submit', addTask)
+todoList.addEventListener('click', deleteTask)
+
+// create a function add
+function addTask(event){
+  event.preventDefault();
+  // create a list
+  let todoLi = document.createElement('li')
+  todoLi.innerText = todoInput.value
+  todoLi.classList.add('todo-list');
+  // create delete button
+  let delBtn= document.createElement('delete');
+  delBtn.innerHTML = '<i class="fa-solid fa-trash"></i>'
+  delBtn.classList.add('todo-btn');
+  todoLi.appendChild(delBtn)
+  // append document
+  todoList.appendChild(todoLi);
+
+  // clear value
+  todoInput.value = ''
+}
+function deleteTask(e){
+  const item = e.target;
+  if(item.classList[0] === 'todo-btn'){
+    const todoRemove = item.parentElement;
+    todoRemove.remove()
+  }
+
+}
 });
